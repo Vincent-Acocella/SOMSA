743f5042b01d915dcd893663687d8380f315dcd7
Anthony Griggs <Anthony.Griggs1@marist.edu>
2020-11-13 19:14:32 -0800

    Refined code and more dilligent testing.

--- src/MST_Twitter.py
+++ src/MST_Twitter.py
@@ -27,7 +27,8 @@ class MSTTwitterSpider(Spider):
 
     current_home_url = 'http://example.com/'
 
-    AMOUNT_OF_PAGE_SCROLLS = 20
+    AMOUNT_OF_PAGE_SCROLLS = 100
+    TRENDS_TO_SCRAPE = 10
 
     def do_scroll(self, page_element, num_scrolls):
         for i in range(num_scrolls):
@@ -73,23 +74,37 @@ class MSTTwitterSpider(Spider):
         self.driver.get(self.current_home_url)
         sleep(0.9)
 
+        # Scroll downwards to load more trends before we select them all
+        # We need something to focus on for scrolling
+        interactable = self.driver.find_element_by_xpath('//*[@data-testid="AppTabBar_Explore_Link"]')
+        for i in range(3):
+            interactable.send_keys(Keys.PAGE_DOWN)
+            sleep(0.5)
+        for i in range(3):
+            interactable.send_keys(Keys.PAGE_UP)
+
         select = Selector(text=self.driver.page_source)
         # Get trends on the page
-        trends_selection = select.xpath('//div[@data-testid="trend"]/@class')
+        trends_selection = select.xpath('//div[@data-testid="trend"]')
+        self.logger.info(trends_selection)
+        #"//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/section/div/div/div[6]"
+        a = 0
+        if a == 0:
+            pass
         trends = trends_selection.getall()
 
         trend_path = '//div[@class="css-1dbjc4n"]'
         print(trends_selection)
         print(trends)
-        for i in range(3):
+        for i in range(self.TRENDS_TO_SCRAPE):
 
             interactable = self.driver.find_element_by_xpath('//*[@data-testid="AppTabBar_Explore_Link"]')
-            self.do_scroll(interactable, 5 * i)
+            self.do_scroll(interactable, 3 * i)
             next_trend = trends[i]
             xpath = '//[@data-test'
 
             # At i = 0, this would be //div[@class="css_1dbjc4n"]/div/div[5]/div/div
-            trend = trend_path + '/div/div[' + str(i + 5) + ']/div/div'
+            trend = trend_path + '/div/div[' + str(i + 5) + ']'
             trend_selection = select.xpath(trend_path + '/div/div[' + str(i + 5) + ']/div/div')
             span = trend_selection.xpath('.//span[@class="css-901oao css-16my406 r-1qd0xha r-ad9z0x r-bcqeeo r-qvutc0"]/text()').getall()
             title = ""
