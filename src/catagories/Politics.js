c9ceae3ffab3666b905e5575756cf6c80306e9d4
vincentacocella <vincent.acocella1@marist.edu>
2020-11-23 02:39:22 -0500

    Updated Format

--- src/catagories/Politics.js
+++
@@ -1,9 +0,0 @@
-import React from './node_modules/react'
-
-export default function Politics() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import Header from './components/Header';
-
-ReactDOM.render(<React.StrictMode><App /></React.StrictMode>,document.getElementById('root'));
-ReactDOM.render(<React.StrictMode><Header /></React.StrictMode>,document.getElementById('signIn'));
-
