b3049f083d7a49677defaac0620cc441f44e1aea
Anthony Griggs <Anthony.Griggs1@marist.edu>
2020-11-25 13:33:53 -0800

    Test of calling all 3 spiders

--- server/pipeline/pipeline.js
+++ server/pipeline/pipeline.js
@@ -44,18 +44,37 @@ callExec = async function (argument) {
 }
 
 onRequest = async function() {
-    //topics = await topic.findAll();
-    topics = ["Earthbound", "US Election", "Joe Biden", "Mother 3"]
-    var spider_arg = ""
+   /* topics = await topic.findAll({
+        attributes: ['Topic_Name', 'Category']
+    });*/
+    //For testing purposes we will use this simple table
+    topics = ["Major League Baseball", "US Election", "Joe Biden", "Playstation 5"]
+    var spider_arg = "trend="
     for(var i = 0; i < topics.length; i++) {
         //We need to send the topics into the spider in a way that it can work with them, replace white space with underscores and concatenate by a period
+        //topics[i] would become topics[i].Topic_Name
         var portion = topics[i];
-        portion = portion.replace(" ", "_");
+        portion = portion.replace(new RegExp(" ", "g"), "_");
         spider_arg += portion + ".";
     }
     console.log(spider_arg);
 
-    //process_db = exec()
+    process_db = exec("scrapy runspider -o ../../machinelearning/data_out.json -a " + spider_arg + " ../../webscraper/MST_Trend_Lookup.py", 
+        {maxBuffer: 1024 * 2000}, (error, stdout, stderr) => {
+    });
+
+    process_db.on('exit', async function() {
+        //Run the reddit scraper
+        process_reddit = exec("scrapy runspider -o ../../machinelearning/data_out.json ../../webscraper/MST_Reddit.py",
+        {maxBuffer: 1024 * 2000}, (error, stdout, stderr) => {
+        });
+        process_reddit.on('exit', async function() {
+            //Run the twitter scraper
+            process_reddit = exec("scrapy runspider -o ../../machinelearning/data_out.json ../../webscraper/MST_Twitter.py",
+            {maxBuffer: 1024 * 2000}, (error, stdout, stderr) => {
+            });
+        })
+    })
 
     //process_reddit = exec("scrapy runspider -o ../../machinelearning/data_out.json ../../webscraper/MST_Reddit.py");
 
