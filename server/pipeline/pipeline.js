f43b4a0262eca5ff1fed503bdaaa1dfb2dc01b6f
Anthony Griggs <Anthony.Griggs1@marist.edu>
2020-11-25 09:57:59 -0800

    Initial demonstration of loading topicS from the database

--- server/pipeline/pipeline.js
+++ server/pipeline/pipeline.js
@@ -1,13 +1,16 @@
 const Sequalize = require('sequelize');
 const db = require('../conf.d/database');
+const topic = require('../models/topic');
 const {exec} = require("child_process");
 const { on } = require('process');
 
-callExec = function (argument) {
+var isAnalyzerReady = true;
+
+callExec = async function (argument) {
 
     //Call the Reddit Spider and parse the output
     //scrapy runspider -o data_out.json MST_Reddit.py
-    process = exec(argument, {maxBuffer: 1024 * 1000}, (error, stdout, stderr) => {
+    process = exec(argument, {maxBuffer: 1024 * 2000}, (error, stdout, stderr) => {
         if (error) {
             console.log(`error: ${error.message}`);
             return 0;
@@ -18,19 +21,48 @@ callExec = function (argument) {
         }
 
     });
+    process.on('exit', await async function() {
 
-    process.on('exit', function() {
+    })
+    /*process.on('exit', function() {
         console.log("Call " + argument + " complete");
-    });
+        while(!isAnalyzerReady) {
+            console.log();
+        }
+        //Either callExec, or send a POST to Flask depending on what we elect to do
+        //With the latter option I'm not entirely sure how we'll know when our response is ready
+        //With the former method, we invoke exec, set isAnalyzerReady to false
+        //then use another process.on 'exit' to know when the SA is done and set isAnalyzerReady to true
+        //That way, we're only calling the SA one at a  time
+        //This *shouldn't* cause any issues with holding the entire backend since this while loops is asynchronous
+        isAnalyzerReady = false;
+        isAnalyzerReady = true;
+        return 1
+        
+    });*/
     return 1;
 }
 
 onRequest = async function() {
-    var a = await callExec("scrapy runspider -o ../../machinelearning/data_out.json ../../webscraper/MST_Reddit.py");
-    console.log(a);
-    a = await callExec("scrapy runspider -o ../../machinelearning/data_out.json ../../webscraper/MST_Twitter.py");
-    console.log(a);
-    console.log("Test");
+    //topics = await topic.findAll();
+    topics = ["Earthbound", "US Election", "Joe Biden", "Mother 3"]
+    var spider_arg = ""
+    for(var i = 0; i < topics.length; i++) {
+        //We need to send the topics into the spider in a way that it can work with them, replace white space with underscores and concatenate by a period
+        var portion = topics[i];
+        portion = portion.replace(" ", "_");
+        spider_arg += portion + ".";
+    }
+    console.log(spider_arg);
+
+    //process_db = exec()
+
+    //process_reddit = exec("scrapy runspider -o ../../machinelearning/data_out.json ../../webscraper/MST_Reddit.py");
+
+   // process_reddit.on('exit', function () {
+
+    //})
+    //callExec("scrapy runspider -o ../../machinelearning/data_out.json ../../webscraper/MST_Twitter.py");
 }
 
 onRequest();
