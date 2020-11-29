fe355b6ec4917f0ba83fc93221812e45c4aa397a
Anthony Griggs <Anthony.Griggs1@marist.edu>
2020-11-29 11:07:40 -0800

    Sentiment table represented in sequelize

---
+++ server/models/sentiment.js
@@ -0,0 +1,30 @@
+const Sequalize = require('sequelize');
+const db = require('../conf.d/database');
+
+const Sentiment = db.define('Sentiment', {
+
+    Sentiment_ID:{
+        type: Sequalize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+
+    },
+    
+    Sentiment: {
+        type: Sequalize.BOOLEAN
+    },
+
+    Confidence_Interval: {
+        type: Sequalize.INTEGER
+    },
+
+    Table_Data: {
+        type: Sequalize.JSON
+    }
+},
+{
+    freezeTableName: true,
+    timestamps: false
+})
+
+module.exports = Sentiment;
